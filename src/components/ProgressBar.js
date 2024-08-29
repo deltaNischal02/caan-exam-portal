395e0bb02d03d408c835ebc0bc157cfa28ee540d
Nischal <nees2076@gmail.com>
2024-08-29 20:48:40 +0545

    progress bar added

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,43 @@
+// src/components/ProgressBar.js
+
+import React from 'react';
+import styled from 'styled-components';
+import { useUserProgress } from '../contexts/UserProgressContext';
+
+const ProgressBarContainer = styled.div`
+  width: 100%;
+  background-color: #e0e0df;
+  border-radius: 10px;
+  margin: 1rem 0;
+`;
+
+const Progress = styled.div`
+  width: ${({ progress }) => progress}%;
+  background-color: #76c7c0;
+  height: 20px;
+  border-radius: 10px;
+  transition: width 0.5s;
+`;
+
+const ProgressText = styled.p`
+  text-align: center;
+  margin-top: 0.5rem;
+`;
+
+const ProgressBar = () => {
+  const { progress } = useUserProgress();
+  const completionRate = (progress.completedQuizzes / progress.totalQuizzes) * 100;
+
+  return (
+    <>
+      <ProgressBarContainer>
+        <Progress progress={completionRate}></Progress>
+      </ProgressBarContainer>
+      <ProgressText>
+        {progress.completedQuizzes}/{progress.totalQuizzes} Quizzes Completed
+      </ProgressText>
+    </>
+  );
+};
+
+export default ProgressBar;
